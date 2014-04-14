b99888b94c7ea480d6f63dd9c9874d51146ca6a0
bawish <barretts@gmail.com>
2014-04-14 16:05:56 -0400

    added db connect/disconnect methods to routes.py

--- routes.py
+++ routes.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template
+import os
+import sqlite3
+from flask import Flask, render_template, request, g
 
+#initialize the Flask object
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+#change config settings from default
+app.config.update(dict(
+  DATABASE=os.path.join(app.root_path, 'salsa_votes.db'),
+  DEBUG=True,
+  SECRET_KEY='123456',
+  USERNAME='admin',
+  PASSWORD='default'
+))
+
+def connect_db():
+  #Connects to the specific database
+  rv = sqlite3.connect(app.config['DATABASE'])
+  rv.row_factory = sqlite3.Row #treats rows like dicts
+  return rv
+
+def get_db():
+  #Opens db connection if one doesn't exist
+  if not hasattr(g, 'sqlite_db'):
+    g.sqlite_db = connect_db()
+  return g.sqlite_db
+
+#i think this executes at the end of every request
+# (indeed, that's what the teardown marker is for)
+#if process executes properly, no error occurs
+#if error occurs, it's passed to this method
+@app.teardown_appcontext
+def close_db(error):
+  if hasattr(g, 'sqlite_db'): #checks to see if db conn exists
+    g.sqlite_db.close()
 
 @app.route('/')
 def vote():
   return render_template('vote.html')
 
 if __name__ == '__main__':
-  app.run(debug=True)  
+  app.run()
