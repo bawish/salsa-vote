9643cd84ed8a086ba5f82ec208f64353917fc56c
bawish <barretts@gmail.com>
2014-04-15 11:56:29 -0400

    updates schema with salsa, votes, users tables

--- schema.sql
+++ schema.sql
@@ -1,6 +1,18 @@
-drop table if exists entries;
-create table entries (
-  id integer primary key autoincrement,
-  title next not null,
-  'text' text not null
+CREATE TABLE IF NOT EXISTS salsas (
+  salsa_id integer primary key autoincrement,
+  salsa_url text not null,
+  type text not null
+);
+
+CREATE TABLE IF NOT EXISTS users (
+  user_id integer primary key autoincrement,
+  user_email text not null
+);
+
+CREATE TABLE IF NOT EXISTS votes (
+  vote_id integer primary key autoincrement,
+  user_id integer not null,
+  salsa_id integer not null,
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (salsa_id) REFERENCES salsas(salsa_id)
 );
